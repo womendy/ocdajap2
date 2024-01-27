563883c0c659b44dfb65b51485196a4e9de2c6df
solange <solange;mendy@gmail;com>
2024-01-27 09:57:50 +0100

    Add interfaces used in home page

--- src/app/core/models/Olympic.ts
+++ src/app/core/models/Olympic.ts
@@ -1,9 +1,5 @@
-// TODO: create here a typescript interface for an olympic country
-/*
-example of an olympic country:
-{
-    id: 1,
-    country: "Italy",
-    participations: []
+interface Olympic {
+  id: number;
+  country: string;
+  participations: Participation[]
 }
-*/
